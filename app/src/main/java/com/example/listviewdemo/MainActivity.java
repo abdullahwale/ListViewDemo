c01bf32625af48c02a901b5586076f7c56a43aa0
abdullahwale <abdullahwale@gmai.com>
2020-02-13 15:23:44 +0500

    Initial commit

---
+++ app/src/main/java/com/example/listviewdemo/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.listviewdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends ListActivity implements AdapterView.OnItemClickListener {
+    String[] courseNames = {"Android Developer Course", "iOS Developer Course", "Java Developer Course",
+            "Objective-C Developer Course", "Web Developer Course"};
+    ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        adapter = new ArrayAdapter<String>(MainActivity.this, R.layout.activity_main, courseNames);
+
+        setListAdapter(adapter);
+        ListView lstView = getListView();
+        lstView.setOnItemClickListener(MainActivity.this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        String itemValue = parent.getItemAtPosition(position).toString();
+        Toast.makeText(MainActivity.this, itemValue, Toast.LENGTH_SHORT).show();
+    }
+}
